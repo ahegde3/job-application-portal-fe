ab2518e753118a2d7437818ec4a3f6d82349669c
ahegde3 <ahegde3@outlook.com>
2023-11-04 21:18:37 -0400

    basic login page done

---
+++ src/component/Logo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div
+      style={{
+        fontFamily: "initial",
+        fontSize: "xx-large",
+        justifyContent: "center",
+        display: "flex",
+        paddingTop: "6px",
+      }}
+    >
+      Job Application Portal
+    </div>
+  );
+}
