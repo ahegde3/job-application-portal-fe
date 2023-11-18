e4accc6fe66dc6340c79583993cbf8c99a506ea2
ahegde3 <ahegde3@outlook.com>
2023-11-18 17:12:31 -0500

    sign up page created

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Login from "./screen/Login";
 import Home from "./screen/Home";
 import Opening from "./screen/Opening";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Registration from "./screen/Registration";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
         <Route path="/home" element={<Home />} />
         <Route path="/opening" element={<Opening />} />
       </Routes>
--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -118,7 +118,11 @@ export default function Login() {
                 </Link>
               </Grid> */}
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link
+                    // href="#"
+                    variant="body2"
+                    onClick={() => navigate("/registration")}
+                  >
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
